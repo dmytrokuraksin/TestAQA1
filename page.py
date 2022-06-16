0c1eb6c07378a62df3cd93feee9537797f2f4921
dmytrokuraksin <dmytro.kuraksin@gmail.com>
2022-06-16 15:07:15 +0300

    Commit 2

--- page.py
+++ page.py
@@ -7,39 +7,35 @@ class BasePage(object):
 class MainPage(BasePage):
 
     def use_search_form(self,id_product):
-        self.driver.implicitly_wait(20)
         element = self.driver.find_element(*PageLocators.search_form_path)
         element.send_keys(id_product)
 
     def click_search_button(self):
-        self.driver.implicitly_wait(20)
         element1 = self.driver.find_element(*PageLocators.search_button_path)
         element1.click()
 
     def go_to_basket(self):
-        self.driver.implicitly_wait(20)
         element2 = self.driver.find_element(*PageLocators.basket_path)
         element2.click()
+        return Basket(self.driver)
 
 class ProductPage(BasePage):
 
     def click_buy_button(self):
-        self.driver.implicitly_wait(20)
         element3 = self.driver.find_element(*PageLocators.buy_button_path)
         element3.click()
-
-    def click_close_button(self):
-        self.driver.implicitly_wait(20)
-        element4 = self.driver.find_element(*PageLocators.close_button_path)
-        element4.click()
+        return Basket(self.driver)
 
     def return_to_mainpage(self):
-        self.driver.implicitly_wait(20)
         element5 = self.driver.find_element(*PageLocators.main_page_path)
         element5.click()
+        return MainPage(self.driver)
 
 class Basket(BasePage):
     def find_product_title(self):
-        self.driver.implicitly_wait(20)
         element6 = self.driver.find_element(*PageLocators.product_title)
         return element6
+
+    def click_close_button(self):
+        element4 = self.driver.find_element(*PageLocators.close_button_path)
+        element4.click()
--- tests/test_rozetka.py
+++ tests/test_rozetka.py
@@ -2,12 +2,14 @@ import unittest
 from selenium import webdriver
 import page
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 class RozetkaTest(unittest.TestCase):
 
     id_prod = 'MD506Z/A'
     def setUp(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.implicitly_wait(30)
         self.driver.get("https://rozetka.com.ua/")
 
     def test_rozetka(self):
@@ -15,11 +17,14 @@ class RozetkaTest(unittest.TestCase):
         product_page = page.ProductPage(self.driver)
         main_page.use_search_form(self.id_prod)
         main_page.click_search_button()
-        """product_page.return_to_mainpage()"""
-        """product_page.click_buy_button()"""
-        """basket_page = page.Basket(self.driver)
+        time.sleep(5)
+        basket_page = product_page.click_buy_button()
+        basket_page.click_close_button()
+        product_page.return_to_mainpage()
+        main_page.go_to_basket()
+
         title = basket_page.find_product_title()
-        assert self.id_prod in title.text"""
+        assert self.id_prod in title.text
 
     def tearDown(self):
         self.driver.close()
