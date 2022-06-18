60d11fc9faaa5070fd65852352f41c61727231c2
dmytrokuraksin <dmytro.kuraksin@gmail.com>
2022-06-18 18:28:44 +0300

    Рабочая версия 1.6 добвлены комментарии

--- tests/test_rozetka.py
+++ tests/test_rozetka.py
@@ -5,12 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class RozetkaTest(unittest.TestCase):
 
-    id_prod = 'MD506Z/A' #Код товара для поиска
+    id_prod = 'MD506Z/A' #id product for search
 
     def setUp(self):
-        #Инициализация драйвера, добавление опций
+        #Driver init
         options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized") #аргумент '--headless' для теста без UI
+        options.add_argument("--start-maximized")
         options.add_argument("--disable-notifications")
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         self.driver.implicitly_wait(10)
@@ -19,14 +19,14 @@ class RozetkaTest(unittest.TestCase):
     def test_rozetka(self):
         main_page = page.MainPage(self.driver)
         product_page = page.ProductPage(self.driver)
-        main_page.use_search_form(self.id_prod) #Ввод кода товара в поисковую строку
-        main_page.click_search_button() #Нажатие кнопки поиска
-        basket_page = product_page.click_buy_button() #Нажатие кнопки купить
-        basket_page.click_close_button() #Нажатие кнопки закрыть окно корзины
-        product_page.return_to_mainpage() #Возврат на главную страницу
-        main_page.go_to_basket() #Переход на страницу корзины
-        title = basket_page.find_product_title() #Поиск названия товара в корзине
-        assert self.id_prod in title #Проверка добавления товара в корзину
+        main_page.use_search_form(self.id_prod) #Find by id_product through search panel
+        main_page.click_search_button() #Click on search button
+        basket_page = product_page.click_buy_button() #Click on buy button
+        basket_page.click_close_button() #Close shopping cart popup
+        product_page.return_to_mainpage() #Return to main page
+        main_page.go_to_basket() #go to shopping cart
+        title = basket_page.find_product_title()
+        assert self.id_prod in title #check the product is added
 
     def tearDown(self):
         self.driver.close()
